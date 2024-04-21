30e47d49962f407b3a038b65d6b3a9db8d3ff3e6
Renaat Debleu <rdebleu@eWallah.net>
2024-04-21 17:22:13 +0000

    version update

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->requires = 2023042400;
 $plugin->component = 'report_payments';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [402, 404];
-$plugin->release = 'v4.2.1';
-$plugin->version = 2024031000;
+$plugin->release = 'v4.4.1';
+$plugin->version = 2024042100;
